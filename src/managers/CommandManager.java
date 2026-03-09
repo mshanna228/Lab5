5dd213b4aa17854850d52b49e318c095f2925169
hanna28 <mrshanna613@gmail.com>
2026-03-09 11:37:49 +0300

    commandManager 01

--- src/managers/CommandManager.java
+++ src/managers/CommandManager.java
@@ -20,12 +20,53 @@ public class CommandManager {
         System.out.println("Программа готова к работе. Введите 'help' для справки.");
         while (true) {
             System.out.println("> ");
-            if (!scanner.hasNextLine()) {
-                break;
+            if (!scanner.hasNextLine()) break;
+
+            String input = scanner.nextLine().trim();
+            if (input.isEmpty()) continue;
+
+            String[] tokens = input.split("\\s+", 2);
+            String command = tokens[0].toLowerCase();
+
+            switch (command) {
+                case "help":
+                    printHelp();
+                    break;
+
+                case "info":
+                    collectionManager.info();
+                    break;
+
+                case "show":
+                    collectionManager.show();
+                    break;
+
+                case "add":
+                    // сюда добавить метод
+                    System.out.println("Запуск нового рабочего...");
+                    break;
+
+
             }
 
 
-            String input = scanner.nextLine();
+//            case "execute_script":
+//                if (tokens.length < 2) {
+//                    System.out.println("Ошибка: введите имя файла после команды!");
+//                } else {
+//                    String fileName = tokens[1];
+//                    // вызываем метод выполнения скрипта
+//                }
+//                break;
+        }
+        private void printHelp() {
+            System.out.println("Доступные команды:");
+            System.out.println(" help : вывести справку по доступным командам");
+            System.out.println(" info : вывести информацию о коллекции");
+            System.out.println(" show : вывести все элементы коллекции");
+            System.out.println(" add : добавить новый элемент в коллекцию");
+            System.out.println(" clear : очистить коллекцию");
+            System.out.println(" exit : завершить программу");
         }
     }
 }
