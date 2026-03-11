b1655f78dc8af4653c2022ddd90eae4f9bf2691c
hanna <mrshanna613@gmail.com>
2026-03-11 18:16:55 +0300

    пока что это промежуточный финальный результат (работает)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import managers.*;
 import io.*;
+import worker.*;
+
+import java.util.Comparator;
 import java.util.Scanner;
 
 /**
@@ -12,19 +15,25 @@ public class Main {
             System.exit(1);
         }
 
+//        var people = new ArrayList<Worker>();
+//        people.add(new Worker("Tom", new Coordinates(3, 4.0), 66, new Position(LABORER), new Status(HIRED), new Organization(GOVERNMENT)));
+//        people.add(new Worker("Harry", new Coordinates(4, 5.0), 36, new Position(LABORER), new Status(HIRED), new Organization(GOVERNMENT)));
+//        Comparator<Worker> totalComporator = new
+
+
         String fileName = args[0];
         FileManager fileManager = new FileManager(fileName);
         WorkerManager workerManager = new WorkerManager(fileManager);
-        
+
         // Автоматическое заполнение из файла
         workerManager.getCollection().addAll(fileManager.readCollection());
 
         ConsoleInputManager consoleInputManager = new ConsoleInputManager(new Scanner(System.in));
         WorkerReader workerReader = new WorkerReader(consoleInputManager);
         ScriptReader scriptReader = new ScriptReader();
-        
+
         CommandManager commandManager = new CommandManager(workerManager, consoleInputManager, workerReader, scriptReader);
-        
+
         commandManager.interactiveMode();
     }
 }
